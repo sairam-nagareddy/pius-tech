7aa97c8099b8501a3c820d5fb3fbba8bc79803ac
spruha.chitturu <developer.spruha@gmail.com>
2023-02-17 23:39:05 +0530

    Added clients

--- src/components/Clients/Clients.jsx
+++ src/components/Clients/Clients.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import styles from './Clients.module.css'
+import amazon from '../../assets/amazon.png'
+import ebay from '../../assets/Ebay.png'
+import walmart from '../../assets/Walmart.png'
+import etsy from '../../assets/Etsy.png'
 
 const Clients = () => {
   return (
-    <div>Clients</div>
+    <div className={styles.clientsContainer}>
+      <div>
+        <img src={amazon} alt="" />
+      </div>
+      <div>
+        <img src={ebay} alt="" />
+      </div>
+      <div>
+        <img src={walmart} alt="" />
+      </div>
+      <div>
+        <img src={etsy} alt="" />
+      </div>
+    </div>
   )
 }
 
