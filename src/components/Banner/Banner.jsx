158a2de9d633c6a6fac47598318864137033e71d
spruha.chitturu <developer.spruha@gmail.com>
2023-02-17 23:34:56 +0530

    Banner added.

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import styles from './Banner.module.css'
-import buddha from '../../assets/bannerImage.png'
+import buddha from '../../assets/buddha2.png'
 
 const Banner = () => {
     return (
         <div className={styles.banner}>
             <img src={buddha} className={styles.buddha}></img>
             <div className={styles.bannerContent}>
-                <span className='titleText'>Complete the E-Commerce Circle with </span><span className='titleText bold'>us.</span>
-                <p>Marketplace management with warehouse partners
+                <div className={styles.bannerTitle}>
+                <span className={'titleText'}>Complete the E-Commerce Circle with </span><span className='titleText bold'>us.</span>
+                </div>
+                <p className={styles.bannerText + ' commonText'}>Marketplace management with warehouse partners<br></br>
                     across the globe. <br></br>
-                    We care deeply about the success of your business on various marketplaces.
+                    We care deeply about the success of your business on <br></br>various marketplaces.
                 </p>
+                <button className='CTA' style={{position:'relative', top:'100px'}}>Join us</button>
             </div>
         </div>
     )
