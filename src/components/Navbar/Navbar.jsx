113a1e2242d7469bf1a6fa49c67c6ce2ec0de4bb
spruha.chitturu <developer.spruha@gmail.com>
2023-02-17 22:25:33 +0530

    Added common classes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styles from './Navbar.module.css'
+import logo from '../../assets/logo.png'
+
 
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
       <div className={styles.logo}>
-        <img></img>
+        <img src={logo}></img>
       </div>
     </div>
   )
