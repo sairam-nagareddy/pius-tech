b22a25fa7dad8b56fe6a6bb0c0493c171c6581b7
spruha.chitturu <developer.spruha@gmail.com>
2023-02-18 15:43:37 +0530

    Banner changes and Footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,30 @@ import styles from './Footer.module.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.footer}>
+      <div className={styles.footerMain}>
+        <div className={styles.col1}>
+            <span className='bold titleText'>PiUS TECH</span>,
+            <br></br>
+            Jai Taramani, E5/15, Bangur Nagar,
+            <br></br>
+            Goregoan West,
+            <br></br>
+            Mumbai, Maharashtra 400104,
+            <br></br>
+            India
+        </div>
+        <div className={styles.col3}>
+          <span className='bold'>Website Policy</span>
+          <br></br>
+          <br></br>
+          <span className='bold'>Privacy Policy</span>
+        </div>
+      </div>
+      <div className={styles.disclaimer}>
+        <p>Disclaimer: We have no affiliation with Amazon, Fulfillment by Amazon (FBA) or its affiliates and partners, including but not limited to; Amazon.in, Amazon.ca, Amazon.uk, Amazon.fr and Amazon.cn, Walmart, eBay, etsy.  Trademarks of these companies are their owners.</p>
+      </div>
+    </div>
   )
 }
 
