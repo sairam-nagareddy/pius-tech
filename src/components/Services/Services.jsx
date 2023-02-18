6287de3671c49afebc6fa1716cad9a55b8636d4c
sairam-nagareddy <sairam.reddy@live.com>
2023-02-18 10:23:24 +0530

    Services Page First Commit

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './Services.module.css'
+import NavigationDots from '../Navbar/NavigationDots'
 
 const Services = () => {
+  const [isActive, setIsActive] = useState(false);
   return (
-    <div>Services</div>
+    <div className={style.servicesContainer}>
+      <div className={style.servicesTitle}>
+        <span className="titleText">Why PiUS</span>
+      </div>
+      <div className={style.infoContainer}>
+        <div className={style.servicesSubtitle}>
+          <span className={'titleText'}>Marketplace Strategy</span>
+        </div>
+        <p className={style.servicesInfo + ' commonText'}>
+          We get to know your business so that we can suggest the best Amazon business model for you. If you’re already selling on Amazon, we’ll let you know if it makes sense to change course to a different business model or maintain course with your current one.
+        </p>
+      </div>
+      <div className='navigation'>
+        <NavigationDots active={isActive}/>
+      </div>
+
+    </div>
   )
 }
 
