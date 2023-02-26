096acd63bc6fef05ba2077ae6d94d40319ad5a1e
spruha.chitturu <developer.spruha@gmail.com>
2023-02-26 15:27:22 +0530

    added title to servicesGrid

--- src/components/Services/ServicesGrid.jsx
+++ src/components/Services/ServicesGrid.jsx
@@ -4,24 +4,29 @@ import services from './data.json'
 
 const ServicesGrid = () => {
     return (
-        <div className={styles.servicesGrid}>
-            {services.map((service, index) => {
-                return (
-                    <div className={styles.serviceItem}>
-                        <div className={styles.serviceIcon} dangerouslySetInnerHTML={{__html: service.image}}>
-                        </div>
-                        <div className={styles.serviceContent}>
-                            <div className={styles.serviceTitle}>
-                                <span>{service.title}</span>
+        <>
+            <div className={styles.sectionTitle}>
+                <span>What we do</span>
+            </div>
+            <div className={styles.servicesGrid}>
+                {services.map((service, index) => {
+                    return (
+                        <div className={styles.serviceItem}>
+                            <div className={styles.serviceIcon} dangerouslySetInnerHTML={{ __html: service.image }}>
                             </div>
-                            <div className={styles.serviceText}>
-                                <p>{service.info}</p>
+                            <div className={styles.serviceContent}>
+                                <div className={styles.serviceTitle}>
+                                    <span>{service.title}</span>
+                                </div>
+                                <div className={styles.serviceText}>
+                                    <p>{service.info}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                )
-            })}
-        </div>
+                    )
+                })}
+            </div>
+        </>
     )
 }
 
