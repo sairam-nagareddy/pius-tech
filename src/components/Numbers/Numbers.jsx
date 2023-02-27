fd980d194e2747c86aba356d1f5a7b0eb49b7af5
spruha.chitturu <developer.spruha@gmail.com>
2023-02-27 21:57:04 +0530

    Increased count duration for a few

--- src/components/Numbers/Numbers.jsx
+++ src/components/Numbers/Numbers.jsx
@@ -12,22 +12,22 @@ const Numbers = () => {
             <div className={styles.Number}>
                 {/* <span className={styles.NumberValue}>500%</span> */}
                 <div className={styles.countContainer}>
-                    <CountUp className={styles.NumberValue} end={300}></CountUp><span className={styles.NumberValue}>%</span>
+                    <CountUp className={styles.NumberValue} end={300} duration={5}></CountUp><span className={styles.NumberValue}>%</span>
                 </div>
                 <span className={styles.NumberTag}>Average Turnover Increase</span>
             </div>
             <div className={styles.Number}>
-                <CountUp className={styles.NumberValue} end={3} duration={1}></CountUp>
+                <CountUp className={styles.NumberValue} end={3} duration={2}></CountUp>
                 <span className={styles.NumberTag}>Accolades Received</span>
             </div>
             <div className={styles.Number}>
                 <div className={styles.countContainer}>
-                    <span className={styles.NumberValue}>$</span><CountUp className={styles.NumberValue} end={100}></CountUp><span className={styles.NumberValue}>M</span>
+                    <span className={styles.NumberValue}>$</span><CountUp className={styles.NumberValue} end={100} duration={5}></CountUp><span className={styles.NumberValue}>M</span>
                 </div>
                 <span className={styles.NumberTag}>Annual Revenue Managed</span>
             </div>
             <div className={styles.Number}>
-                <CountUp className={styles.NumberValue} end={5} duration={1}></CountUp>
+                <CountUp className={styles.NumberValue} end={5} duration={2}></CountUp>
                 <span className={styles.NumberTag}>Certifications</span>
             </div>
         </div>
