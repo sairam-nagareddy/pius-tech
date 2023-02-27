71d3b774ca3969758beec6d11bf4c20af5660651
sairam-nagareddy <sairam.reddy@live.com>
2023-02-27 20:44:13 +0530

    Final changes second commit

--- src/components/Numbers/Numbers.jsx
+++ src/components/Numbers/Numbers.jsx
@@ -12,17 +12,17 @@ const Numbers = () => {
             <div className={styles.Number}>
                 {/* <span className={styles.NumberValue}>500%</span> */}
                 <div className={styles.countContainer}>
-                    <CountUp className={styles.NumberValue} end={500}></CountUp><span className={styles.NumberValue}>%</span>
+                    <CountUp className={styles.NumberValue} end={300}></CountUp><span className={styles.NumberValue}>%</span>
                 </div>
-                <span className={styles.NumberTag}>Average Profit Increase</span>
+                <span className={styles.NumberTag}>Average Turnover Increase</span>
             </div>
             <div className={styles.Number}>
                 <CountUp className={styles.NumberValue} end={3} duration={1}></CountUp>
-                <span className={styles.NumberTag}>Awards Received</span>
+                <span className={styles.NumberTag}>Accolades Received</span>
             </div>
             <div className={styles.Number}>
                 <div className={styles.countContainer}>
-                    <span className={styles.NumberValue}>$</span><CountUp className={styles.NumberValue} end={50}></CountUp><span className={styles.NumberValue}>M</span>
+                    <span className={styles.NumberValue}>$</span><CountUp className={styles.NumberValue} end={100}></CountUp><span className={styles.NumberValue}>M</span>
                 </div>
                 <span className={styles.NumberTag}>Annual Revenue Managed</span>
             </div>
